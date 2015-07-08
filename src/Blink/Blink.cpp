78bd7615b17080c334a955a80bc84dea56643f8e
Jordan Rhee <jordanrh@microsoft.com>
2015-07-07 18:00:45 -0700

    Reverting Blink.cpp to master version.

--- src/Blink/Blink.cpp
+++ src/Blink/Blink.cpp
@@ -6,22 +6,15 @@
  */
 #include <Arduino.h>
 
-// use PB0 (Digital 8)
-
 void setup() {                
   // initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards:
-  //pinMode(13, OUTPUT);     
-  // DDRx controls direction (1 = output, 0 = input)
-  // PORTx must be 0 to switch off pull up resistor
-  // swithc DDR between 0 and 1
-  PORTB &= ~0x1;
-  DDRB &= ~0x1;
+  pinMode(13, OUTPUT);     
 }
 
 void loop() {
-  DDRB |= 0x1;
-  delay(100);
-  DDRB &= ~0x1;
-  delay(1000);
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(500);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(500);              // wait for a second
 }
